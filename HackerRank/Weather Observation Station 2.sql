15d6e9668caeb11297c4e104cd1eec6316c46a6e
Neslihan Yetik <ytkneslihan@gmail.com>
2021-09-02 13:43:05 +0300

    SQL Solutions

---
+++ HackerRank/Weather Observation Station 2.sql
@@ -0,0 +1,8 @@
+/* Weather Observation Station 2
+
+We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as  space-separated integers.
+
+*/
+
+SELECT ROUND(SUM(LAT_N), 2), ROUND(SUM(LONG_W), 2)
+FROM STATION;
