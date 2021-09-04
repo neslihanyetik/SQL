cbb6f5148e37433597ac5f1e85dba0ec3b92d449
Neslihan Yetik <ytkneslihan@gmail.com>
2021-09-04 13:24:50 +0300

    SQL Solutions

---
+++ HackerRank/Weather Observation Station 14.sql
@@ -0,0 +1,7 @@
+/* Weather Observation Station 14
+Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than . Truncate your answer to  decimal places.
+*/
+
+SELECT ROUND(MAX(LAT_N),4)
+FROM STATION
+WHERE LAT_N < 137.2345
