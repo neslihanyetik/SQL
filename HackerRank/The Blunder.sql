304329f4db61df756137e65fdd137fbc49d40549
Neslihan Yetik <ytkneslihan@gmail.com>
2021-09-19 17:52:39 +0300

    SQL Solutions

---
+++ HackerRank/The Blunder.sql
@@ -0,0 +1,16 @@
+/*
+The Blunder
+Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, 
+but did not realize her keyboard's 0 key was broken until after completing the calculation. 
+She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), 
+and the actual average salary.
+Write a query calculating the amount of error(i.e.:average monthly salaries), and round it up to the next integer.
+*/
+
+
+SELECT
+    ROUND(AVG(SALARY)) -
+    ROUND(AVG(REPLACE(SALARY,'0','')))
+FROM EMPLOYEES;
+
+
