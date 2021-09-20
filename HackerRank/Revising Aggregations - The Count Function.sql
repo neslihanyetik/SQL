851777443bcdae9c32ad249d701636a907b123e9
Neslihan Yetik <ytkneslihan@gmail.com>
2021-09-20 11:04:38 +0300

    SQL Solutions

---
+++ HackerRank/Revising Aggregations - The Count Function.sql
@@ -0,0 +1,10 @@
+/* Revising Aggregations - The Count Function
+
+Query a count of the number of cities in CITY having a Population larger than 100.000
+
+*/
+
+
+SELECT COUNT(ID)
+FROM CITY
+WHERE POPULATION >100000;
